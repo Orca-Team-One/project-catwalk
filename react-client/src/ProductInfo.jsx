04d3a47ad5b206b9810d0bfdcf8eee4ff4070186
Tyna Nguyen <ngtyna@gmail.com>
2020-11-12 19:04:21 -0600

    Testing out feature branch workflow with a new component file

---
+++ react-client/src/ProductInfo.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ProductInfo = () => {
+  return (
+    <div>
+      <p>This component includes product category, product name, price, and star ratings.</p>
+    </div>
+  );
+}
+
+export default ProductInfo;
