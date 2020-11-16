ab6b13d2f3063313d55dbaf0206f5ea552dd809b
Tyna Nguyen <ngtyna@gmail.com>
2020-11-15 22:09:43 -0600

    Add subcomponents to the ProductOverview component

---
+++ react-client/src/ProductOverview/AddToCart.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function AddToCart() {
+  return (
+    <div>
+      <p>Add to Cart Button</p>
+      <p>Favorite Button</p>
+    </div>
+  );
+}
+
+export default AddToCart;
