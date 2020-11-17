c9c45613f41afab3bf795443de66c9eebaf89785
Tyna Nguyen <ngtyna@gmail.com>
2020-11-17 17:29:15 -0600

    Render dummy data for the overall product overview component

--- react-client/src/ProductOverview/ProductInfo.jsx
+++ react-client/src/ProductOverview/ProductInfo.jsx
@@ -1,15 +1,35 @@
 import React from 'react';
 
-function ProductInfo() {
+function ProductInfo(props) {
   return (
     <div>
-      <p>This component includes product category, product name, price, and star ratings.</p>
+      {/* Star Rating, Read all Reviews
+        ==> User can click on "Read all reviews" and is taken to Ratings and Reviews section
+        ==> Check if there are ratings
+            If there are no ratings, render empty stars
+            If there are ratings, calculate the average for all ratings
+            Render the average of stars filled in
+      */}
       <p>Star Ratings</p>
       <p>Read all reviews</p>
-      <p>Product Category</p>
-      <p>Product Name</p>
-      <p>Product Price or Sale Price</p>
-      <p>Social Media Links</p>
+
+      {/* Render the selected product's category */}
+      <p>Product Category: {props.product.category}</p>
+
+      {/* Render the selected product's name */}
+      <p>Product Name: {props.product.name}</p>
+
+      {/* Render the selected product's price
+        ==> Check if there is a sale price from /styles
+            If the sale price is 0, render the original price
+            If the sale price is greater than 0,
+              Render the original price with a strike-through,
+              Render the sale price
+      */}
+      <p>Product Sale Price: ${props.details.results[0]["sale_price"]}</p>
+      <p>Product Original Price: ${props.details.results[0]["original_price"]}</p>
+
+      {/* <p>Social Media Links</p> */}
     </div>
   );
 }
