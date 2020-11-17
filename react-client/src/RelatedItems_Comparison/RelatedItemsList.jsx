ef98caa395118b17fa5e4425a9106a93d5dd1e99
Jesse Perez <jessep1130@gmail.com>
2020-11-17 10:39:03 -0600

    currently rendering all cards for related Items

--- react-client/src/RelatedItems_Comparison/RelatedItemsList.jsx
+++ react-client/src/RelatedItems_Comparison/RelatedItemsList.jsx
@@ -2,11 +2,18 @@ import React, { Component } from 'react'
 import RelatedItemsEntry from './RelatedItemsEntry.jsx'
 
 export default class RelatedItemsList extends Component {
+
+
   render() {
+    console.log(this.props.relatedItems);
     return (
       <div>
-        Related Items!
-        <RelatedItemsEntry/>
+        Related Items
+        {
+          this.props.relatedItems.map((relatedEntry, index) => (
+            <RelatedItemsEntry key = {index} relatedEntries = {this.props.relatedItems[index]}/>
+          ))
+        }
       </div>
     )
   }
--- react-client/src/RelatedItems_Comparison/RelatedItems_Comparison.jsx
+++ react-client/src/RelatedItems_Comparison/RelatedItems_Comparison.jsx
@@ -1,15 +1,22 @@
 import React, { Component } from 'react'
 import ComparisonItemsList from './ComparisonItemsList.jsx'
 import RelatedItemsList from './RelatedItemsList.jsx'
+import dummyData from './dummyData.js'
 
 
 export default class RelatedItems_Comparison extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      RelatedItems: dummyData
+    }
+  }
   render() {
     return (
       <div>
         Hello from RelatedItems_Comparison
         <ComparisonItemsList/>
-        <RelatedItemsList/>
+        <RelatedItemsList relatedItems = {this.state.RelatedItems}/>
       </div>
     )
   }
