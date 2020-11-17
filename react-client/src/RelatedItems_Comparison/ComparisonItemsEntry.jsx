0a4da325aa9a1ef3af97ce0ca91a2b316d069146
Jesse Perez <jessep1130@gmail.com>
2020-11-17 15:24:01 -0600

    whole cards are now links - will work on shrinking

--- react-client/src/RelatedItems_Comparison/ComparisonItemsEntry.jsx
+++ react-client/src/RelatedItems_Comparison/ComparisonItemsEntry.jsx
@@ -3,12 +3,13 @@ import React, { Component } from 'react'
 export default class ComparisonItemsEntry extends Component {
   render() {
     return (
-      <div class="comparison_card align_cards">
+      <div class="container card comparison_card alignRelatedCards"><a href="#" class="stretched-link">
         <img src="https://picsum.photos/id/237/200/300" alt="Denim Jeans" ></img>
         <p><a href="#">{this.props.comparedEntries.name}</a></p>
         <p>{this.props.comparedEntries.category}</p>
         <p class="card_price">${this.props.comparedEntries.default_price}</p>
         <p>Ratings</p>
+        </a>
       </div>
     )
   }
--- react-client/src/RelatedItems_Comparison/RelatedItemsEntry.jsx
+++ react-client/src/RelatedItems_Comparison/RelatedItemsEntry.jsx
@@ -4,13 +4,14 @@ export default class RelatedItemsEntry extends Component {
   render() {
     console.log(this.props.relatedEntries);
     return (
-      <div class="related_card align_cards">
+      <div class="container card related_card alignComparedCards"><a href="#" class="stretched-link">
         <img src="https://picsum.photos/id/1025/200/300" alt="Denim Jeans" ></img>
-        <p><a href="#">{this.props.relatedEntries.name}</a></p>
+        <p>{this.props.relatedEntries.name}</p>
         <p>{this.props.relatedEntries.category}</p>
         <p class="card_price">${this.props.relatedEntries.default_price}</p>
         <p>Ratings</p>
         {/* <p><button>Add to Cart</button></p> */}
+        </a>
       </div>
     )
   }
