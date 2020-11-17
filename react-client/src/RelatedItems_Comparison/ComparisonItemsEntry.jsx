c499059bd5ac5e260ed6682ceac8e94cf8c6ccb6
Jesse Perez <jessep1130@gmail.com>
2020-11-17 12:21:34 -0600

    added images and links

--- react-client/src/RelatedItems_Comparison/ComparisonItemsEntry.jsx
+++ react-client/src/RelatedItems_Comparison/ComparisonItemsEntry.jsx
@@ -4,12 +4,11 @@ export default class ComparisonItemsEntry extends Component {
   render() {
     return (
       <div class="comparison_card align_cards">
-        {/* <img src="https://www.gstatic.com/webp/gallery3/2.png" alt="Denim Jeans" style="width:100%"></img> */}
-        <p><a href="#">{this.props.comparedEntries.category}</a></p>
-        <p>{this.props.comparedEntries.name}</p>
+        <img src="https://picsum.photos/id/237/200/300" alt="Denim Jeans" ></img>
+        <p><a href="#">{this.props.comparedEntries.name}</a></p>
+        <p>{this.props.comparedEntries.category}</p>
         <p class="card_price">${this.props.comparedEntries.default_price}</p>
         <p>Ratings</p>
-        {/* <p><button>Add to Cart</button></p> */}
       </div>
     )
   }
--- react-client/src/RelatedItems_Comparison/RelatedItemsEntry.jsx
+++ react-client/src/RelatedItems_Comparison/RelatedItemsEntry.jsx
@@ -5,9 +5,9 @@ export default class RelatedItemsEntry extends Component {
     console.log(this.props.relatedEntries);
     return (
       <div class="related_card align_cards">
-        {/* <img src="https://www.gstatic.com/webp/gallery3/2.png" alt="Denim Jeans" style="width:100%"></img> */}
-        <p><a href="#">{this.props.relatedEntries.category}</a></p>
-        <p>{this.props.relatedEntries.name}</p>
+        <img src="https://picsum.photos/id/1025/200/300" alt="Denim Jeans" ></img>
+        <p><a href="#">{this.props.relatedEntries.name}</a></p>
+        <p>{this.props.relatedEntries.category}</p>
         <p class="card_price">${this.props.relatedEntries.default_price}</p>
         <p>Ratings</p>
         {/* <p><button>Add to Cart</button></p> */}
