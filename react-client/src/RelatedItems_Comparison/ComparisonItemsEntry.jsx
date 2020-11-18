ca15f98b18d75178b2286b3089de94b7b548f8d4
Jesse Perez <jessep1130@gmail.com>
2020-11-18 10:20:08 -0600

    changed some camel case stuff

--- react-client/src/RelatedItems_Comparison/ComparisonItemsEntry.jsx
+++ react-client/src/RelatedItems_Comparison/ComparisonItemsEntry.jsx
@@ -5,7 +5,7 @@ export default class ComparisonItemsEntry extends Component {
     return (
       <div class="container card comparison_card alignRelatedCards"><a href="#" class="stretched-link">
         <img src="https://picsum.photos/id/237/200/300" alt="Denim Jeans" ></img>
-        <p><a href="#">{this.props.comparedEntries.name}</a></p>
+        <p>{this.props.comparedEntries.name}</p>
         <p>{this.props.comparedEntries.category}</p>
         <p class="card_price">${this.props.comparedEntries.default_price}</p>
         <p>Ratings</p>
