cf2ab8d448a5a4cb7354b5104f560a26b3e52776
Tyna Nguyen <ngtyna@gmail.com>
2020-11-19 15:04:11 -0600

    Update styling with React Bootstrap

--- react-client/src/ProductOverview/SizeAndQuantity.jsx
+++ react-client/src/ProductOverview/SizeAndQuantity.jsx
@@ -1,32 +1,36 @@
 import React from 'react';
+// Testing React Bootstrap
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 function Selectors(props) {
   let skuNums = Object.keys(props.details.results[0].skus); // [1, 2, 3, 4, 5, 6]
   let quantities = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
   return (
-    <div>
+    <Row>
       {/* Size Selector */}
-      <div>
-        <label htmlFor="size" placeholder="Select size:">Select size: </label>
-        <select name="size" className="sizeSelector">
+      <Col sm={3}>
+        <label>Select size: </label>
+        <select className="selector">
           {skuNums.map((num) =>
             <option>{props.details.results[0].skus[`${num}`].size}</option>
           )}
 
         </select>
-      </div>
+      </Col>
 
       {/* Quantity Selector */}
-      <div>
-        <label htmlFor="size">Select quantity: </label>
-        <select name="size" className="sizeSelector">
+      <Col>
+        <label>Select quantity: </label>
+        <select className="selector">
           {quantities.map((quantity) =>
             <option>{quantity}</option>
           )}
         </select>
-      </div>
-    </div>
+      </Col>
+    </Row>
   );
 }
 
