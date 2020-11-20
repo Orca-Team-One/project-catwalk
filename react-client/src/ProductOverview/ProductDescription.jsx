02b1059306accbe330e96b652b4b638f659b7799
Tyna Nguyen <ngtyna@gmail.com>
2020-11-20 11:11:31 -0600

    Add conditional rendering for product features in Product Description component

--- react-client/src/ProductOverview/ProductDescription.jsx
+++ react-client/src/ProductOverview/ProductDescription.jsx
@@ -4,19 +4,26 @@ import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 
-function ProductDescription({ product, info }) {
+function ProductDescription({ productData }) {
   return (
     <Container className="productDescription">
       <Row>
         <Col lg={8}>
-          <h5>{product.slogan}</h5>
-          <p>{product.description}</p>
+          <h5>{productData.slogan}</h5>
+          <p>{productData.description}</p>
         </Col>
         <Col lg={4}>
-          <h5>Product Features </h5>
-          {info.features.map((feature) =>
-            <p>{feature.value} {feature.feature}</p>
-          )}
+          <h5>Product Features</h5>
+          { !productData.features
+            ? <p>Loading...</p>
+            : productData.features.map((feature) => {
+              return (
+                <div>
+                  <p>{feature.value} {feature.feature}</p>
+                </div>
+              )
+            })
+          }
         </Col>
       </Row>
     </Container>
