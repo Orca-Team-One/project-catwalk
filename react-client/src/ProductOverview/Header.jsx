a7e061f1fabe20aa9c32605b38ad01759bc77e07
Tyna Nguyen <ngtyna@gmail.com>
2020-11-20 23:36:24 -0600

    Create a Header component

---
+++ react-client/src/ProductOverview/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import SearchIcon from '@material-ui/icons/Search';
+
+function Header() {
+  return (
+    <div className="header">
+      <p>Logo</p>
+      <input>Searchbar Icon</input>
+    </div>
+  );
+}
+
+export default Header;
