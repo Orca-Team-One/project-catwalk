b312d01b52570f3f1e23de3e6c1b0496c828abb8
Tyna Nguyen <ngtyna@gmail.com>
2020-11-21 14:59:17 -0600

    Refactor Product View component to be a class component

--- react-client/src/ProductOverview/ProductView.jsx
+++ react-client/src/ProductOverview/ProductView.jsx
@@ -3,27 +3,37 @@ import ProductImageView from './ProductImageView.jsx';
 import ProductDetailsColumn from './ProductDetailsColumn.jsx';
 import ProductDescription from './ProductDescription.jsx';
 
-function ProductView({ productID, productData, productStyles, currentStyle }) {
-  return (
-    <div className="productViewContainer">
-      { Object.keys(productData).length === 0 || productStyles.length === 0 || Object.keys(currentStyle).length === 0
-        ? <p>Loading...</p>
-        : <div>
-            <ProductImageView
-              productID={productID}
-              productStyles={productStyles}
-              currentStyle={currentStyle}/>
-            <ProductDetailsColumn
-              productID={productID}
-              productData={productData}
-              productStyles={productStyles}
-              currentStyle={currentStyle}/>
-            <ProductDescription
-              productData={productData}/>
-          </div>
-      }
-    </div>
-  );
+class ProductView extends Component {
+  // props from App { productID, productData, productStyles, currentStyle }
+
+  constructor(props) {
+    super(props);
+
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div className="productViewContainer">
+        { Object.keys(this.props.productData).length === 0 || Object.keys(this.props.currentStyle).length === 0
+          ? <p>Loading...</p>
+          : <div>
+              <ProductImageView
+                productID={this.props.productID}
+                productStyles={this.props.productStyles}
+                currentStyle={this.props.currentStyle}/>
+              <ProductDetailsColumn
+                productID={this.props.productID}
+                productData={this.props.productData}
+                productStyles={this.props.productStyles}
+                currentStyle={this.props.currentStyle}/>
+              <ProductDescription
+                productData={this.props.productData}/>
+            </div>
+        }
+      </div>
+    );
+  }
 }
 
 export default ProductView;
