9231b51e4c4fe31bf69287120a74371863a76ba5
Tyna Nguyen <ngtyna@gmail.com>
2020-11-21 17:13:21 -0600

    Update styling for ProductImageView and ProductThumbnailColumn components

--- react-client/src/ProductOverview/ProductImageView.jsx
+++ react-client/src/ProductOverview/ProductImageView.jsx
@@ -1,21 +1,25 @@
 import React from 'react';
-import { Figure, FigureImage} from 'react-bootstrap'
+import { Row, Col, Figure, FigureImage} from 'react-bootstrap'
 import ProductThumbnailsColumn from './ProductThumbnailsColumn.jsx';
 
 function ProductImageView({ productID, productStyles, selectedStyle }) {
   return (
     <div className="productImageView">
-      <Figure className="currentImageView">
-        <Figure.Image
-          width={500}
-          height={1100}
-          src={productStyles[selectedStyle].photos[0].url} />
-      </Figure>
-
-      {/* Product Thumbnails */}
-      <ProductThumbnailsColumn
-        productStyles={productStyles}
-        selectedStyle={selectedStyle}/>
+      <Row>
+        <Col sm={1}>
+          <ProductThumbnailsColumn
+            productStyles={productStyles}
+            selectedStyle={selectedStyle}/>
+        </Col>
+        <Col sm={8}>
+          <Figure className="currentImageView">
+            <Figure.Image
+              width={500}
+              height={1100}
+              src={productStyles[selectedStyle].photos[0].url} />
+          </Figure>
+        </Col>
+      </Row>
     </div>
   );
 }
