30d07473397ed78255c07929f6ae4c88ea6d0ef0
Tyna Nguyen <ngtyna@gmail.com>
2020-11-21 11:30:44 -0600

    Update conditional rendering for ProductInfo component

--- react-client/src/ProductOverview/ProductInfo.jsx
+++ react-client/src/ProductOverview/ProductInfo.jsx
@@ -5,26 +5,24 @@ function ProductInfo({ productID, productData, productStyles }) {
   return (
     <div>
       {/* Star Rating, Read all Reviews */}
-      <p>* * * * * | <a href="">Read all # reviews</a></p>
+      <p>* * * * * | <a href="#ratingsReviewsComponent">Read all # reviews</a></p>
 
       {/* Product Category and Name */}
-      { productData.category
-        ? <div>
+      { Object.keys(productData).length === 0 || productStyles.length === 0
+        ? <p>Loading...</p>
+        : <div>
             <p>{productData.category.toUpperCase()}</p>
             <h5>{productData.name}</h5>
-          </div>
-        : <p>Loading...</p>
-      }
 
-      {/* Product Price, Sale Price */}
-      { productStyles.length !== 0
-        ? productStyles[0]["sale_price"] !== '0'
-          ? <div>
-              <p style={{textDecoration: 'line-through'}}>${productStyles[0]["original_price"]}</p>
-              <p>${productStyles[0]["sale_price"]}</p>
-            </div>
-          : <p>Original Price: ${productStyles[0]["original_price"]}</p>
-        : <p>Loading...</p>
+            {/* Product Price, Sale Price */}
+            { productStyles[0]["sale_price"] === '0'
+              ? <p>Original Price: ${productStyles[0]["original_price"]}</p>
+              : <div>
+                  <p style={{textDecoration: 'line-through'}}>${productStyles[0]["original_price"]}</p>
+                  <p>${productStyles[0]["sale_price"]}</p>
+                </div>
+            }
+          </div>
       }
 
       {/* <p>Social Media Links</p> */}
