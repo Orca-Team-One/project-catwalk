35462b7af71da8b552f296b87c9dc6941f0798ab
Tyna Nguyen <ngtyna@gmail.com>
2020-11-21 13:23:56 -0600

    Remove conditional rendering for props within lower components

--- react-client/src/ProductOverview/StyleSelector.jsx
+++ react-client/src/ProductOverview/StyleSelector.jsx
@@ -6,19 +6,14 @@ import { Container, Row, Col } from 'react-bootstrap';
 function StyleSelector ({ productID, productStyles }) {
   return (
     <div>
-      { productStyles.length !== 0
-        ? <div>
-            <h6>STYLE > {productStyles[0].name}</h6>
-            <Row>
-              {productStyles.map((result, index) =>
-                <Col sm={2} key={index}>
-                  <Avatar src={result.photos[0].thumbnail_url} />
-                </Col>
-              )}
-            </Row>
-          </div>
-        : <p>Loading...</p>
-      }
+      <h6>STYLE > {productStyles[0].name.toUpperCase()}</h6>
+      <Row>
+        {productStyles.map((result, index) =>
+          <Col sm={2} key={index}>
+            <Avatar src={result.photos[0].thumbnail_url} />
+          </Col>
+        )}
+      </Row>
     </div>
   );
 }
