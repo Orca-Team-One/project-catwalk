bd7ad3678fc0b48a0cc9c9eb453880736b528e60
Tyna Nguyen <ngtyna@gmail.com>
2020-11-21 13:49:16 -0600

    Remove dummy data file imports

--- react-client/src/ProductOverview/ProductView.jsx
+++ react-client/src/ProductOverview/ProductView.jsx
@@ -2,10 +2,6 @@ import React, { Component } from 'react';
 import ProductImageView from './ProductImageView.jsx';
 import ProductDetailsColumn from './ProductDetailsColumn.jsx';
 import ProductDescription from './ProductDescription.jsx';
-// Test rendering with dummy data
-import sampleProducts from './sampleData/sampleProducts.js';
-import sampleDetails from './sampleData/sampleDetails.js';
-import sampleInfo from './sampleData/sampleInfo.js';
 
 function ProductView({ productID, productData, productStyles }) {
   return (
