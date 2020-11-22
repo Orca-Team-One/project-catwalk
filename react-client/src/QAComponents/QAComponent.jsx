591e545f6b8a2645654f3b843a529c1dd3e04214
Philip Woram <philip.woram@gmail.com>
2020-11-22 13:22:10 -0600

    Moved the logic for sorting questions by helpfulness and displaying two at a time up one component to the main QandAComponent.

--- react-client/src/QAComponents/QAComponent.jsx
+++ react-client/src/QAComponents/QAComponent.jsx
@@ -1,21 +1,46 @@
 import React, { Component } from 'react'
 import QASearchBar from './QASearchBar.jsx';
-
+import MoreAnsweredQuestions from './MoreAnsweredQuestions.jsx';
+import AddAQuestion from './AddAQuestion.jsx';
 import QuestionList from './QuestionList.jsx';
 
 export default class QAComponent extends Component {
   constructor(props){
     super(props);
+    this.state = {
+      questionsToShow: 2,
+      showMore: true,
+    }
+    this.handleClick = this.handleClick.bind(this);
+  }
 
+  handleClick() {
+    this.setState(prevState => ({
+      questionsToShow: prevState.questionsToShow + 2
+    }));
+    if (this.state.questionsToShow < this.props.productQuestions.length || this.props.productQuestions.length <= 2) {
+      this.setState(prevState => ({
+        showMore: !prevState.showMore,
+      }))
+    }
   }
 
   render() {
     //destructuring assignment
     const {productQuestions} = this.props;
+    const {questionsToShow} = this.state;
+
+    //a copy of questions array takes the number of questions to be displayed from question array passed from the parent
+    const copyOfQuestions = productQuestions.slice(0, questionsToShow);
+    console.log('copyOfQuestions:', copyOfQuestions)
+
     //sorting the questions array by their helpfulness rating
-    const sortedArray = productQuestions.sort(function(a, b) {
+    const sortedArray = copyOfQuestions.sort(function(a, b) {
       return b.question_helpfulness - a.question_helpfulness;
       });
+
+    console.log('sortedArray:', sortedArray)
+
     return (
       <>
       <div className="askWidgetContainer">
@@ -30,7 +55,22 @@ export default class QAComponent extends Component {
             <QuestionList questions={sortedArray}/>
           </div>
       </div>
-
+      <div className="container">
+          <div className="row">
+            <div className="col-">
+            {this.state.showMore ?
+              <button onClick={this.handleClick}>
+              More Answered Questions
+              </button> :
+              <>
+              </>
+            }
+            </div>
+            <div className="col-">
+            <AddAQuestion/>
+            </div>
+          </div>
+        </div>
       </>
     )
   }
--- react-client/src/QAComponents/QuestionList.jsx
+++ react-client/src/QAComponents/QuestionList.jsx
@@ -1,68 +1,26 @@
 import React, { Component } from 'react';
 import Question from './Question.jsx';
 import exampleQAData from './QADummyData.js';
-import MoreAnsweredQuestions from './MoreAnsweredQuestions.jsx';
-import AddAQuestion from './AddAQuestion.jsx';
+
 
 export default class QuestionList extends Component {
   constructor(props){
     super(props);
-    this.state = {
-      questionsToShow: 2,
-      showMore: true,
-    }
-    this.handleClick = this.handleClick.bind(this);
-  }
-
-  handleClick() {
-    this.setState(prevState => ({
-      questionsToShow: prevState.questionsToShow + 2
-    }));
-    if (this.state.questionsToShow < this.props.questions.length) {
-      this.setState(prevState => ({
-        showMore: !prevState.showMore,
-      }))
-    }
   }
 
   render() {
     //destructuring assingment
     const {questions} = this.props;
-    const {questionsToShow} = this.state;
-
-    //a copy of questions array takes the number of questions to be displayed from question array passed from the parent
-    const copyOfQuestions = questions.slice(0, questionsToShow);
-    console.log('copyOfQuestions:', copyOfQuestions)
-    //the display questions array maps over the copy of Questions
-
-    // console.log('display questions', displayQuestions)
-
 
     return (
       <>
-        {copyOfQuestions.map((question) =>
+        {questions.map((question) =>
           <div class="container" style={{
             paddingTop: "15px",
           }}>
           <Question question={question} key={question.id}/>
           </div>
         )}
-        <div className="container">
-          <div className="row">
-            <div className="col-">
-            {this.state.showMore ?
-              <button onClick={this.handleClick}>
-              More Answered Questions
-              </button> :
-              <>
-              </>
-            }
-            </div>
-            <div className="col-">
-            <AddAQuestion/>
-            </div>
-          </div>
-          </div>
       </>
     )
   }
