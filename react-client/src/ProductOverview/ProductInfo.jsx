d58f6ee95ef344ce85de8e122ff94e67e6c56c01
Tyna Nguyen <ngtyna@gmail.com>
2020-11-23 15:44:44 -0600

    Add a Rating component for product's star ratings (currently static)

--- react-client/src/ProductOverview/ProductInfo.jsx
+++ react-client/src/ProductOverview/ProductInfo.jsx
@@ -1,11 +1,21 @@
 import React from 'react';
+import { Rating } from '@material-ui/lab';
 
 function ProductInfo({ productData, productStyles, selectedStyle }) {
 
   return (
     <div>
       {/* Star Rating, Read all Reviews */}
-      <p>* * * * * | <a href="#ratingsReviewsComponent">Read all # reviews</a></p>
+      <div>
+        <Rating
+          name="simple-controlled"
+          size="small"
+          value={3}
+          defaultValue={0}
+          precision={0.25}
+          readOnly/>
+        <a href="#ratingsReviewsComponent">  Read all # reviews</a>
+      </div>
 
       {/* Product Category and Name */}
       <p>{productData.category.toUpperCase()}</p>
