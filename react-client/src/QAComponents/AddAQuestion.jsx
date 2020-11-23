aa00859d9a0408b111167418abac59fb0947c279
Philip Woram <philip.woram@gmail.com>
2020-11-23 13:07:28 -0600

    Made Add Answer and Add Question modals submit data to the API.

--- react-client/src/QAComponents/AddAQuestion.jsx
+++ react-client/src/QAComponents/AddAQuestion.jsx
@@ -1,54 +1,55 @@
 import React, { Component } from 'react';
-import { Button, Modal} from 'react-bootstrap';
+import { Button, Modal } from 'react-bootstrap';
 import AddQuestionModal from './AddQuestionModal.jsx';
 
-
 export default class AddAQuestion extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      showHide: false
-    }
-  }
+	constructor(props) {
+		super(props);
+		this.state = {
+			showHide: false,
+		};
+	}
 
-  handleModalShowHide() {
-    this.setState({
-      showHide: !this.state.showHide
-    })
-  }
+	handleModalShowHide() {
+		this.setState({
+			showHide: !this.state.showHide,
+		});
+	}
 
-render() {
-  //destructuring assignment
-  const {productName} = this.props;
+	render() {
+		//destructuring assignment
+		const { productName, productID } = this.props;
 
-  return (
-    <div>
-      <button onClick={() => this.handleModalShowHide()}>
-        Add A Question
-      </button>
+		return (
+			<div>
+				<button onClick={() => this.handleModalShowHide()}>
+					Add A Question
+				</button>
 
-    <Modal show={this.state.showHide}>
-        <Modal.Header closeButton onClick={() => this.handleModalShowHide()}>
-          <Modal.Title>Ask Your Question</Modal.Title>
-        </Modal.Header>
-        <Modal.Body>
-          About the {productName}
-        <AddQuestionModal/>
-
-        </Modal.Body>
-        <Modal.Footer>
-          <Button variant="secondary" onClick={() => this.handleModalShowHide()}>
-              Close
-          </Button>
-          <Button variant="primary" onClick={() => this.handleModalShowHide()}>
-              Submit
-          </Button>
-        </Modal.Footer>
-    </Modal>
-    </div>
-  )
-  }
+				<Modal show={this.state.showHide}>
+					<Modal.Header closeButton onClick={() => this.handleModalShowHide()}>
+						<Modal.Title>Ask Your Question</Modal.Title>
+					</Modal.Header>
+					<Modal.Body>
+						About the {productName}
+						<AddQuestionModal productID={productID} />
+					</Modal.Body>
+					<Modal.Footer>
+						<Button
+							variant="secondary"
+							onClick={() => this.handleModalShowHide()}
+						>
+							Close
+						</Button>
+						<Button
+							variant="primary"
+							onClick={() => this.handleModalShowHide()}
+						>
+							Submit
+						</Button>
+					</Modal.Footer>
+				</Modal>
+			</div>
+		);
+	}
 }
-
-
-
--- react-client/src/QAComponents/Answer/Answer.jsx
+++ react-client/src/QAComponents/Answer/Answer.jsx
@@ -4,52 +4,69 @@ import HelpfulButton from '../HelpfulButton.jsx';
 import ReportButton from '../Answer/ReportButton.jsx';
 
 export default class Answer extends Component {
-  constructor(props) {
-    super(props);
-  }
-  render() {
-    // console.log('answer props', this.props)
-    return (
-      <div className="container">
-        <div className="row">
-          <div className="col-" style={{
-            paddingBottom: "5px",
-            paddingTop: "10px",
-          }}>
-            <b style={{
-              paddingRight: "2px",
-            }}>
-              A:
-            </b>
-            {this.props.answer.body}
-          </div>
-        </div>
-          <div className="row">
-            <div className="col-">
-              <UserInfo user={this.props.answer.answerer_name} date={this.props.answer.date}/>
-            </div>
-            <div className="col-" style={{
-              paddingLeft: "5px",
-              paddingRight: "5px",
-            }}>
-            |
-            </div>
-            <div className="col-">
-              <HelpfulButton helpfulCount={this.props.answer.helpfulness}/>
-            </div>
+	constructor(props) {
+		super(props);
+	}
+	render() {
+		// console.log('answer props', this.props)
+		return (
+			<div className="container">
+				<div className="row">
+					<div
+						className="col-"
+						style={{
+							paddingBottom: '5px',
+							paddingTop: '10px',
+						}}
+					>
+						<b
+							style={{
+								paddingRight: '2px',
+							}}
+						>
+							A:
+						</b>
+						{this.props.answer.body}
+					</div>
+				</div>
+				<div className="row">
+					<div className="col-">
+						<UserInfo
+							user={this.props.answer.answerer_name}
+							date={this.props.answer.date}
+						/>
+					</div>
+					<div
+						className="col-"
+						style={{
+							paddingLeft: '5px',
+							paddingRight: '5px',
+						}}
+					>
+						|
+					</div>
+					<div className="col-">
+						<HelpfulButton
+							helpfulCount={this.props.answer.helpfulness}
+							helpfulness={this.props.answer.id}
+						/>
+					</div>
 
-            <div className="col-" style={{
-              paddingLeft: "5px",
-              paddingRight: "5px",
-            }}>
-            |
-            </div>
+					<div
+						className="col-"
+						style={{
+							paddingLeft: '5px',
+							paddingRight: '5px',
+						}}
+					>
+						|
+					</div>
 
-            <div className="col-">
-              <ReportButton/>
-            </div>
-          </div>
-      </div>
-    )
-  }
+					<div className="col-">
+						<ReportButton />
+					</div>
+				</div>
+			</div>
+		);
+	}
 }
