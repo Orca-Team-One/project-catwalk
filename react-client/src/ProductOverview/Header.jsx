6d029c49c879f30a77fae6c7543b1ad42f77e5ea
Tyna Nguyen <ngtyna@gmail.com>
2020-11-27 14:39:35 -0600

    Add a text field for the Search bar

--- react-client/src/ProductOverview/Header.jsx
+++ react-client/src/ProductOverview/Header.jsx
@@ -1,17 +1,22 @@
 import React from 'react';
 import { Row, Col } from 'react-bootstrap';
+import { Grid, FormControl, TextField, IconButton } from '@material-ui/core';
 import SearchIcon from '@material-ui/icons/Search';
 
 function Header() {
   return (
     <div className="header">
       <Row className="logo-text">
-        <Col>
-          <h1>Logo | App Name</h1>
+        <Col sm={8}>
+          <h3>Logo | App Name</h3>
         </Col>
-        <Col>
-          {/* <TextField id="standard-basic" label="Standard" /> */}
-          <SearchIcon className="search-icon" color="inherit" fontSize="large"/>
+        <Col sm={2}>
+          <TextField id="standard-basic" label="Search"/>
+        </Col>
+        <Col sm={2}>
+          <IconButton>
+            <SearchIcon className="search-icon" color="inherit" fontSize="medium"/>
+          </IconButton>
         </Col>
       </Row>
       <p className="announcements"><em>SITE-WIDE ANNOUNCEMENT MESSAGE!</em> - SALE / DISCOUNT <b>OFFER</b> - NEW PRODUCT HIGHLIGHT</p>
