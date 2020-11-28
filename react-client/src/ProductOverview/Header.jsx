b6aac6a9dc569bc27f3304d40bd9fd9cdae2f823
Tyna Nguyen <ngtyna@gmail.com>
2020-11-28 01:50:31 -0600

    Update Header searchbar and add shopping cart icon

--- react-client/src/ProductOverview/Header.jsx
+++ react-client/src/ProductOverview/Header.jsx
@@ -1,25 +1,46 @@
 import React from 'react';
 import { Row, Col } from 'react-bootstrap';
-import { Grid, FormControl, TextField, IconButton } from '@material-ui/core';
+import { Grid, FormControl, TextField, InputAdornment, IconButton, Badge } from '@material-ui/core';
 import SearchIcon from '@material-ui/icons/Search';
+import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
 
 function Header() {
   return (
-    <div className="header">
-      <Row className="logo-text">
-        <Col sm={8}>
-          <h3>Logo | App Name</h3>
-        </Col>
-        <Col sm={2}>
-          <TextField id="standard-basic" label="Search"/>
-        </Col>
-        <Col sm={2}>
-          <IconButton>
-            <SearchIcon className="search-icon" color="inherit" fontSize="medium"/>
-          </IconButton>
-        </Col>
-      </Row>
-      <p className="announcements"><em>SITE-WIDE ANNOUNCEMENT MESSAGE!</em> - SALE / DISCOUNT <b>OFFER</b> - NEW PRODUCT HIGHLIGHT</p>
+    <div className="header-container">
+      <div className="header">
+        <Row className="logo-text">
+          <Col>
+            <h3>Logo | App Name</h3>
+          </Col>
+          <Col xs="auto">
+            <TextField
+              id="standard-basic"
+              margin="dense"
+              placeholder="Search"
+              fullWidth
+              InputProps={{
+                endAdornment: (
+                  <InputAdornment>
+                    <IconButton>
+                      <SearchIcon color="inherit" />
+                    </IconButton>
+                  </InputAdornment>
+                )
+              }}/>
+          </Col>
+          <Col xs="auto">
+            <IconButton>
+              <Badge badgeContent={0} color="primary">
+                <ShoppingCartIcon/>
+              </Badge>
+            </IconButton>
+          </Col>
+          <Col xs="auto"></Col>
+        </Row>
+      </div>
+      <div className="announcements">
+        <p><em>SITE-WIDE ANNOUNCEMENT MESSAGE!</em> - SALE / DISCOUNT <b>OFFER</b> - NEW PRODUCT HIGHLIGHT</p>
+      </div>
     </div>
   );
 }
