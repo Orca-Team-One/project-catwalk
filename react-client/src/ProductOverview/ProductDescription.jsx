a60ef3c3523a7516713fc9ff022f96d7732f2bfe
Tyna Nguyen <ngtyna@gmail.com>
2020-11-30 00:44:16 -0600

    Add styling for Product Description component

--- react-client/src/ProductOverview/ProductDescription.jsx
+++ react-client/src/ProductOverview/ProductDescription.jsx
@@ -3,12 +3,12 @@ import { Container, Row, Col } from 'react-bootstrap';
 
 function ProductDescription({ productData }) {
   return (
-    <div className="productDescription">
+    <div className="product-description">
       <Row>
         <Col sm={1}></Col>
         <Col sm={6}>
-          <h5><b>{productData.slogan}</b></h5>
-          <p>{productData.description}</p>
+          <p className="product-data-slogan"><b>{productData.slogan}</b></p>
+          <p className="product-data-description">{productData.description}</p>
         </Col>
         <Col sm={4}>
           <p className="product-features-title"><b>Product Features</b></p>
@@ -16,7 +16,7 @@ function ProductDescription({ productData }) {
             return (
               <div key={index}>
                 <p className="product-features-list">
-                  <i className="fa fa-check"></i> {''} {feature.feature}: {feature.value}
+                  <i className="fa fa-check"></i> {''} {feature.feature} | {feature.value}
                 </p>
               </div>
             )
